2487f4447cd6b05e73660c5dbbb60b4172b482be
NormanBro <88945718+NormanBro@users.noreply.github.com>
2022-11-27 19:07:49 -0700

    Materias Add

--- src/app/Components/NewMateria/body-add-material/body-add-material.component.ts
+++ src/app/Components/NewMateria/body-add-material/body-add-material.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup,FormControl,Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ServPostService } from 'src/app/Service/ServPost/serv-post.service';
+
 @Component({
   selector: 'app-body-add-material',
   templateUrl: './body-add-material.component.html',
@@ -11,20 +13,45 @@ export class BodyAddMaterialComponent implements OnInit {
     Nombre:new FormControl('',[Validators.required]),
     Avatar:new FormControl('',[Validators.required])
   })
-  constructor(private _snackBar:MatSnackBar) { }
+  constructor(
+    private _snackBar:MatSnackBar,
+    private ServPost:ServPostService
+    ) { }
 
-  FileAvatar!:File;
+  FileAvatar:any;
 
   EntrarMateria(){
+    
     if(this.FormAdd.value.Nombre){
-      console.log(this.FileAvatar);
+
+      const Body=new FormData()
+      Body.append('myFile',this.FileAvatar.fileRaw,this.FileAvatar.fileName)
+      Body.append('Nombre',this.FormAdd.value.Nombre);
+      Body.append('Imagen',this.FileAvatar.fileName || 'default.jpg');
+      Body.append('ID_usuario',sessionStorage.getItem('ID_usuario') || '');
+      
+      /*const Body={
+        Nombre:this.FormAdd.value.Nombre,
+        Imagen:this.FileAvatar||'default.jpg',
+        ID_usuario:sessionStorage.getItem('ID_usuario')
+      }*/
+      
+      this.ServPost.PostAll('materias',Body).subscribe(resp=>{
+        
+      })
+
     }else{
       this._snackBar.open('No se ha agregado nada','Dismiss');
     } 
   }
 
   OnFilesAvatar(e:any){
-    this.FileAvatar=e.target.files;
+    [File]=e.target.files;
+    this.FileAvatar={
+      fileRaw:File,
+      fileName:File.name
+    }
+
   }
 
   ngOnInit(): void {
